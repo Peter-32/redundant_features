45cc65eb40d41a3c06409c98b7bd7d6f3febdd91
Peter-32 <peter@impactradius.com>
2022-05-28 17:56:19 -0700

    init

---
+++ 9.py
@@ -0,0 +1,35 @@
+from numpy import random
+from sklearn.metrics import mean_squared_error
+import pandas as pd
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.datasets import fetch_california_housing
+from numpy.random import seed
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+housing = fetch_california_housing()
+m, n = housing.data.shape
+X_original = pd.DataFrame(housing.data)
+ss = StandardScaler()
+X_original.columns = housing.feature_names
+y = housing.target
+
+for k in [0,1,2,3,4,5,6]:
+    X = X_original.copy()
+    new_data = random.standard_normal((X.shape[0], k))
+    X = pd.concat([X, pd.DataFrame(new_data)], axis='columns', ignore_index=True)
+
+    seed(32)
+    split_size = 0.4 if X.shape[0] < 100000 else 40000/X.shape[0]
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=split_size)
+
+    X_train = ss.fit_transform(X_train, y_train)
+    X_test = ss.transform(X_test)
+
+    model = KNeighborsRegressor(n_neighbors=11)
+    model.fit(X_train, y_train)
+    y_pred = model.predict(X_test)
+    mse = mean_squared_error(y_test, y_pred)
+    if k == 0:
+        first_mse = mse
+    print(k, mse, (first_mse-mse) / first_mse)
